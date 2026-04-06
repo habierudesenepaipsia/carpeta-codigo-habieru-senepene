9fe655ee565e0c9e8d03bc442591228b9cb93beb
HABIERUDESEN <javier.valencia15@inacapmail.cl>
2026-04-06 01:27:21 -0400

    tootooomifaneavelme

--- weacosapaiton.py
+++ weacosapaiton.py
@@ -1,1 +1,14 @@
-print (str (input ("no voy a josear en clase")))
+nota_1 = float(input("cuál es su última nota recordada?: "))
+if nota_1 >= 1.0:
+    if nota_1 <= 7.0:
+        if nota_1 >= 4.0:
+            print("usted ha aprobado.")
+        else:
+            print("usted ha reprobado.")    
+    else:
+        print("valor incorrecto, coloque un valor bajo 7.0")
+        exit()
+else:
+    print("valor incorrecto, coloque un valor sobre 1.0")
+    exit()        
+#el pepe ete sech potaxio los que saben el contexto    
