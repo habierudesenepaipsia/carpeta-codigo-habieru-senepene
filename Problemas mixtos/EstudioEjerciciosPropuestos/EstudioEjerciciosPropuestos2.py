228af8c8896a6ccaf48468a7ae6a0586fdc2dfa5
HABIERUDESEN <javier.valencia15@inacapmail.cl>
2026-04-25 01:22:05 -0400

    sadad

--- Problemas mixtos/EstudioEjerciciosPropuestos/EstudioEjerciciosPropuestos2.py
+++ Problemas mixtos/EstudioEjerciciosPropuestos/EstudioEjerciciosPropuestos2.py
@@ -2,9 +2,9 @@
 #de flujo que muestre el valor de un producto actualizado y la diferencia de precio entre el mes de febrero y Marzo.
 productoing = int(input("ingrese el último valor de su producto:"))
 #supongamos que la wea es 1800
-productofebrero = productoing % 0.3
-productofinfeb = productoing * productofebrero
-productomarzo = productoing / 0.6
-productofinmarzo = productomarzo * 100
+productofebrero = productoing * 0.003
+productofinfeb = productoing + productofebrero
+productomarzo = productoing * 0.006
+productofinmarzo = productomarzo + productoing
 print(f"su producto actualmente cuesta {productoing}, en febrero costaba {productofinfeb}, y en marzo {productofinmarzo}.")
-#PUTA LA WEA ME EQUIVOQUEEE
+#EL CHRISTIAN ES MI SALVADOR, ME HIZO ESTOS PROBLEMAS
--- Problemas mixtos/EstudioEjerciciosPropuestos/EstudioEjerciciosPropuestos3.py
+++ Problemas mixtos/EstudioEjerciciosPropuestos/EstudioEjerciciosPropuestos3.py
@@ -1,2 +1,6 @@
-#Confeccione un algoritmo en diagrama de flujo que al leer el neto de una factura, calcule el I.V.A. y de cómo salida el total de la
-#factura
+#Confeccione un algoritmo en diagrama de flujo que al leer el neto de una factura, calcule el I.V.A. y de cómo salida
+#el total de la factura
+factura = float(input("ingrese su factura:"))
+iva = factura * 0.19
+facturatotal = iva + factura
+print(f"Su total de la factura es {facturatotal}")
