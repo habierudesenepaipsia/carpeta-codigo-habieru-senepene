f0ad2a70bda7aabc4fca693a62d3ee1868a2d165
HABIERUDESEN <javier.valencia15@inacapmail.cl>
2026-04-25 02:00:36 -0400

    putamadreabuela

--- Problemas mixtos/EstudioEjerciciosPropuestos/EstudioEjerciciosPropuestos2.py
+++ Problemas mixtos/EstudioEjerciciosPropuestos/EstudioEjerciciosPropuestos2.py
@@ -1,3 +1,10 @@
 #Un vendedor recibe un sueldo base mas un 10% extra por comisión de sus ventas, el vendedor desea saber cuanto dinero obtendrá
 #por concepto de comisiones por las tres ventas que realiza en el mes y el total que recibirá en el mes tomando en cuenta su sueldo
 #base y comisiones.
+sueldobase = int(input("ingrese su sueldo base:"))
+sueldo3mes = sueldobase * 3
+comisionbase = sueldobase / 10
+comision3mes = comisionbase * 3
+sueldototal = sueldo3mes + comision3mes
+#test
+print(f"Respecto a su sueldo base, usted recibirá {comision3mes} extra en comisiones en este mes, y su sueldo total por las 3 ventas más sus comisiones es {sueldototal}.")
---
+++ Problemas mixtos/EstudioEjerciciosPropuestos/EstudioEjerciciosPropuestos3.py
@@ -0,0 +1,2 @@
+#Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea saber cuanto deberá pagar finalmente por su
+#compra
