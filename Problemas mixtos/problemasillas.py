665e012ba910fdd311ad52e2ddd18bc628880e78
habierudesenapaipsia <javier.valencia15@inacapmail.cl>
2026-04-27 13:57:43 -0400

    CONCHETYNARE adasdaasdasadddsdsdsfddffdadsgthcft

---
+++ Problemas mixtos/problemasillas.py
@@ -0,0 +1,13 @@
+#Desarrolle un sistema que permite ingresar 10 trabajadores, por cada uno de ellos ingresar la cantidad de sillas construidas (sueldo), el cálculo es el siguiente:
+#-Por cada silla construida se le paga 1000 pesos. -Si supera las 2000 sillas construidas le pagaremos 20 pesos más sobre las 2000.
+#El sistema debe entregar la siguiente estadística, al finalizar los 19 trabajadores:
+#A) Total a pagar por todos los trabajadores.
+#B) Total de sillas construidas.
+#C) Total de bono pagado por sillas sobre 2000
+#D) Cuántos trabajadores hicieron más de 2000 sillas.
+#E) Cuanto es el promedio de silla entre todos los trabajadores.
+
+
+
+
+####MEVOYASUICIDAR#####
