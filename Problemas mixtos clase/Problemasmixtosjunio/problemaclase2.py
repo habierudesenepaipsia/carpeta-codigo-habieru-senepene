36ebd5e3e2cb2a4be9da10118b32d1a254d28b66
habierudesenepaipsia <javllancao@gmail.com>
2026-05-25 14:54:16 -0400

    pene

---
+++ Problemas mixtos clase/Problemasmixtosjunio/problemaclase2.py
@@ -0,0 +1,9 @@
+#Desarrollar un programa que permita ingresar 10 números, y al finalizar
+#desplegar por pantalla cuales elementos son mayores al promedio de la lista
+
+numlista = []
+cocos = 0
+
+
+for i in numlista(0,10, 1):
+    cocos = int(input("Ingrese sus números:"))
