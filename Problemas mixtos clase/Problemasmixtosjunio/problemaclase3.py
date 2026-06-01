ca68c684f3a3dee4b08293e4d41874acb0f5a040
habierudesenepaipsia <javier.valencia15@inacapmail.cl>
2026-06-01 14:54:34 -0400

    elpepesech1

--- Problemas mixtos clase/Problemasmixtosjunio/problemaclase3.py
+++ Problemas mixtos clase/Problemasmixtosjunio/problemaclase3.py
@@ -1,2 +1,14 @@
 #Desarrolle un programa que permita ingresar 10 notas (de 1 a 7) en una lista,
 #y al finalizar desplegar el promedio y en que índice se encuentra la nota más alta y más baja
+notasingresadas1 = []
+notas1 = 0
+variablepico1 = 0
+variablex = 0
+mitula1 = 0 
+
+for i in notasingresadas1 (0, 10, 1):
+    notas1 = float(input("Ingrese su siguiente nota:"))
+    
+    #PUTAMADRENOAVANZEXD
+    
+    notasingresadas1 = notasingresadas1 + 1
