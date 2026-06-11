f311b5e7435ad623ee37123c1be0d4e567c1b28f
habierudesenepaipsia <javier.valencia15@inacapmail.cl>
2026-06-11 17:15:45 -0400

    TROLASTE

--- Problemas mixtos clase/Problemasmixtosjunio/problemaclase5.py
+++ Problemas mixtos clase/Problemasmixtosjunio/problemaclase5.py
@@ -1,3 +1,13 @@
 #######FUNCION QUE SE HAGA DESDE AHORA DEBE DECIR QUE HACE Y QUE RETORNA##########
 
 #DESARROLLE UNA FUNCION QUE RECIBA UNA LISTA Y ELIMINE TODOS LOS ELEMENTOS NEGATIVOS DE LA LISTA Y RETORNE LA LISTA ACTUALIZADA
+def cacaca1(list: int) -> list:
+    lisata1 = []
+    for x in list:
+        if x >= 0:
+            lisata1.append(x)
+    return lisata1    
+
+l = cacaca1(1, -2, 3, -4, 5)
+
+print(l)
