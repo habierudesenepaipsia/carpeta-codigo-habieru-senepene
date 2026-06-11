c3d1f75e4b7510d649ac22b12c140d064d994534
habierudesenepaipsia <javier.valencia15@inacapmail.cl>
2026-06-11 16:10:29 -0400

    sdafha appe

--- Problemas mixtos clase/Problemasmixtosjunio/problemaclase4.py
+++ Problemas mixtos clase/Problemasmixtosjunio/problemaclase4.py
@@ -1,2 +1,11 @@
 #Desarrolle una funcion que reciba un numero como argumento
 #que retorne una lista con las cantidades de elementos segun el argumento recibido
+lista1 = []
+ekis = int(input("Ingrese un número x límite:"))
+for n in range (1,ekis + 1):
+    if n % 2 != 0:
+        lista1.append(n)
+for n in range (ekis, 0, -1):
+    if n % 2 == 0:
+        lista1.append(n)
+print(lista1)
