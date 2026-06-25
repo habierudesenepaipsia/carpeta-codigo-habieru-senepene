869e7471e07dc677cb1a8a6d7da25cea9fbde262
habierudesenepaipsia <javier.valencia15@inacapmail.cl>
2026-06-25 16:14:35 -0400

    adssdaadaad

--- Problemas mixtos clase/Problemasmixtosjunio/PROBLEMACLASE6.py
+++ Problemas mixtos clase/Problemasmixtosjunio/PROBLEMACLASE6.py
@@ -1,3 +1,18 @@
-nom = input("ing nombre:")
+#nom = input("ing nombre:")
+#x = 0
+#while x < len(nom):
+#    print(nom[x])
+#    x = x + 1
+#Para que sirven los ar chivos csv
+#nom = input("Ingrese un nombre :")
+#for i in range (len(nom)):
+#
+#    print(nom[i])
+#EL USUARIO INGRESA SU NOMBRE Y NOSOTROS DESPLEGAMOS EL NOMBRE AL REVÉS
+nombre = input("ingrese su nombre:")
+x1 = len(nombre)-1
+x2 = 0
 
-print(nom[0])
+while x1 >= 0:
+    print(nombre[x1], end="")
+    x1 = x1 - 1
