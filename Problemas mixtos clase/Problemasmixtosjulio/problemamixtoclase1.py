de4945f9823db316f20c88be9c21442d17d5b187
habierudesenepaipsia <javier.valencia15@inacapmail.cl>
2026-07-02 16:46:31 -0400

    aadsdadads

---
+++ Problemas mixtos clase/Problemasmixtosjulio/problemamixtoclase1.py
@@ -0,0 +1,35 @@
+#desarrollar un programa que indique hasta donde desea la serie
+#1era regla, la impresion siempre va a ser con 2 numeros
+#2da regla, no se puede pasar de la serie a no ser que los numeros calzen
+#(1,3)
+#(2,4)
+#(5,7)
+#(6,8)
+#(9,11)
+#(10,12)
+#(13,15)
+#(14,16)
+
+input1 = 0
+valor1 = 0
+valor2 = 2
+i1 = 1
+banderaserie = False
+
+serievalor = int(input("Ingrese un valor limite para que la serie acabe:"))
+
+seriefinvalor = 0
+
+while seriefinvalor < serievalor:
+    if i1 % 2 != 0:
+        print(f"({valor1}),({valor2})")
+        valor1 = valor1 + 1 
+        valor2 = valor2 + 1
+
+    if i1 % 2 == 0:
+        print(f"({valor1}),({valor2})")
+        valor1 = valor1 + 3 
+        valor2 = valor2 + 3
+
+    seriefinvalor = seriefinvalor + 1    
+    i1 = i1 + 1
