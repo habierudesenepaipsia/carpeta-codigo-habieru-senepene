d0c23c3044ab31baea84f33b1e871f685cf69197
habierudesenepaipsia <habierudesenepaipsia>
2026-08-11 14:53:19 -0400

    quepajaseguirrconcjetuamre

---
+++ ProblemasEstudioPrueba/Problemasmixtoclasesegsemest/novoyajosearenclase1.py
@@ -0,0 +1,22 @@
+#Desarrolle un programa que agregue a una lista los números ingresados por el usuario que cumplan la siguiente condición:
+#1, debe ser positivo.
+#2, el último dígito debe ser multiplo de 2.
+#3, codigo de salida es 999 
+#Requerimientos
+#A: debe crear una funcion llamada cumple que reciba un argumento númerico (el numero ingresado por el usuario) y retorne verdadero o falso si se debe agregar a la lista
+#Restricciones
+#No puede ocupar ni transformar el número a un string o a una variable cadena, debe trabajar con el número directamente
+
+variablepic = 0
+variablejos = 0
+
+numeroing = int(input("Ingrese el siguiente número:"))
+
+
+
+if numeroing == 999:
+    
+
+
+
+
