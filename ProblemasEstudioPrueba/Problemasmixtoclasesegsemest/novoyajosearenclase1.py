4ae5e20e63da76f766f27c32e7c27a964d016933
habierudesenepaipsia <javllancao@gmail.com>
2026-08-11 22:33:52 -0400

    problema_terminado_11_agosto

--- ProblemasEstudioPrueba/Problemasmixtoclasesegsemest/novoyajosearenclase1.py
+++ ProblemasEstudioPrueba/Problemasmixtoclasesegsemest/novoyajosearenclase1.py
@@ -1,17 +1,36 @@
 #Desarrolle un programa que agregue a una lista los números ingresados por el usuario que cumplan la siguiente condición:
-#1, debe ser positivo.
-#2, el último dígito debe ser multiplo de 2.
-#3, codigo de salida es 999 
+#1, x debe ser positivo.
+#2, x el último dígito debe ser multiplo de 2.
+#3, x codigo de salida es 999 
 #Requerimientos
-#A: debe crear una funcion llamada cumple que reciba un argumento númerico (el numero ingresado por el usuario) y retorne verdadero o falso si se debe agregar a la lista
+#A: x debe crear una funcion llamada cumple que reciba un argumento númerico (el numero ingresado por el usuario) 
+#   y retorne verdadero o falso si se debe agregar a la lista
 #Restricciones
-#No puede ocupar ni transformar el número a un string o a una variable cadena, debe trabajar con el número directamente
+#x No puede ocupar ni transformar el número a un string o a una variable cadena, debe trabajar con el número directamente
 
-variablepic = 0
-variablejos = 0
+def cumple(num: int) -> bool:
+    if num > 0:
+        if (num % 10) % 2 == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
 
-numeroing = int(input("Ingrese el siguiente número:"))
+listnum = []
+num = 0
 
+while num != 999:
+    num = int(input("ingrese el siguiente número:"))
+    num1 = cumple(num)
+    
+    if num1 == True:    
+        listnum.append(num)
+              
+    else:
+        if num == 999:
+            print("lista terminada")
+        else:
+            print("número incorrecto, reingrese") 
 
-
-if numeroing == 999:
+print(listnum)        
