b1b306259d57c1fa0197e1ded7fb23c7c4e33472
habierudesenepaipsia <javier.valencia15@inacapmail.cl>
2026-08-18 14:24:37 -0400

    adadsakld,jafkljdslk

--- Problemasmixtoclasesegsemest/NOVOYAJOSEARENCLASE3.py
+++ Problemasmixtoclasesegsemest/NOVOYAJOSEARENCLASE3.py
@@ -1,16 +1,31 @@
-Celular
+#Celular
 
-Mod       Atributos         Tipo
+#Mod       Atributos         Tipo
 
-get       -IMEI             :int
-get       -Marca            :str
-get       -Almacenamiento   :float
-set       -Nivel_Bateria    :float
-get       -Sis_Op           :str
+#get       -IMEI             :int
+#get       -Marca            :str
+#get       -Almacenamiento   :float
+#set       -Nivel_Bateria    :float
+#get       -Sis_Op           :str
 
-Métodos                      Tipo
+#Métodos                      Tipo
  
-+Prender( )                  :bool
-+Llamar(nmrtelfno)           :int
-+Carga()
-+Celular(Imei{constructors})
+#+Prender( )                  :bool
+#+Llamar(nmrtelfno)           :int
+#+Carga()
+#+Celular(Imei{constructor})
+
+
+
+
+
+
+
+############TAREA##########
+#TRAER LOS 10 OBJETOS EN DIAGRAMA
+
+class Celular:
+    #__privado
+    #_publico
+    def __init__(self, imei): # Indica que es un argumento necesario para instanciar el objeto 
+        self.__imei = imei
