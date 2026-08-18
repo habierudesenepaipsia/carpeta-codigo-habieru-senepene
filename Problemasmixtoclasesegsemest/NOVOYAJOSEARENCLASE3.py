1e70073560f83a90feaf30ce59d46fb86b819637
habierudesenepaipsia <javier.valencia15@inacapmail.cl>
2026-08-18 14:07:54 -0400

    cbxcxvcx

---
+++ Problemasmixtoclasesegsemest/NOVOYAJOSEARENCLASE3.py
@@ -0,0 +1,16 @@
+Celular
+
+Mod       Atributos         Tipo
+
+get       -IMEI             :int
+get       -Marca            :str
+get       -Almacenamiento   :float
+set       -Nivel_Bateria    :float
+get       -Sis_Op           :str
+
+Métodos                      Tipo
+ 
++Prender( )                  :bool
++Llamar(nmrtelfno)           :int
++Carga()
++Celular(Imei{constructors})
